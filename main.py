b4e10cba0132c2cdd320c0995d9eb0ed19981128
ekarabulut19 <72880622+ekarabulut19@users.noreply.github.com>
2021-10-03 18:53:19 +0000

    part1

---
+++ main.py
@@ -0,0 +1,16 @@
+x = int (input("Enter number x: "))
+y = int (input("Enter number y: "))
+
+power = x**y
+print("x**y =", power)
+
+
+
+import numpy
+
+log= int(numpy.log2(x))
+print("log(x) = " , log)
+
+
+
+print("71876")
